142761de64da5af0a31c17228fa048e6ae5050a5
smit patel <smitpatel@clearquote.io>
2023-02-01 13:59:32 +0530

    Updated server -2

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const path = require("path");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 const config = require("./config/config.json");
 const cors = require("cors");
-// require("dotenv").config({
-//   path: path.join(__dirname, "./.env"),
-// });
 
 // connect
 const app = express();
 const PORT = process.env.PORT || 3200;
-mongoose.set("strictQuery", false);
-mongoose
-.connect(config.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Connected to the Database successfully");
-});
+// mongoose.set("strictQuery", false);
+// mongoose
+// .connect(config.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//   console.log("Connected to the Database successfully");
+// });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
